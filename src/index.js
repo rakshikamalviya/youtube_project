68183fb8713095980376e9c4d165b483fb37bf70
Rakshika malviya <rakshikamalviya3@gmail.com>
2026-01-16 13:53:44 +0530

    util code discution

--- src/index.js
+++ src/index.js
@@ -30,3 +30,15 @@ app.use(express.json());
 })();
 
 
+
+
+// .then(() => {
+//     app.listen(process.env.PORT || 3000 => {
+//         console.log(`server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!!", err);
+// })
+
+
